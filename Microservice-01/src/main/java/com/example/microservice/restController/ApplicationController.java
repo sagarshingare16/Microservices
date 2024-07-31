138915f9bc647ed883aa2f79bef4804cd3c8bc29
sagarshingare16 <sagargshingare16@gmail.com>
2024-07-31 23:54:56 +0530

    eureka server

---
+++ Microservice-01/src/main/java/com/example/microservice/restController/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.example.microservice.restController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
