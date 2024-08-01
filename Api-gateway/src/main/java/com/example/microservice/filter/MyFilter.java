042c42970372584e10775fac7bf83d953d846003
sagarshingare16 <sagargshingare16@gmail.com>
2024-08-01 23:30:59 +0530

    api gateway implementation

---
+++ Api-gateway/src/main/java/com/example/microservice/filter/MyFilter.java
@@ -0,0 +1,16 @@
+package com.example.microservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("Filtering");
+        return chain.filter(exchange);
+    }
+}
