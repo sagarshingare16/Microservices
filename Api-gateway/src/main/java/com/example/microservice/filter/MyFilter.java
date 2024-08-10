3379ccfe2bf0556eb82b80308d7ff53c23b93684
sagarshingare16 <sagargshingare16@gmail.com>
2024-08-10 20:32:06 +0530

    finterration

--- Api-gateway/src/main/java/com/example/microservice/filter/MyFilter.java
+++ Api-gateway/src/main/java/com/example/microservice/filter/MyFilter.java
@@ -2,15 +2,25 @@ package com.example.microservice.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Set;
 
 @Component
 public class MyFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("Filtering");
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders httpHeaders = request.getHeaders();
+        Set<String> keySet = httpHeaders.keySet();
+        for (String key : keySet) {
+            System.out.println(key +"--->");
+            System.out.println();
+        }
         return chain.filter(exchange);
     }
 }
