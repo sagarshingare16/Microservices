dd82544848b9c57a5dcb79c888899bf75aab6933
sagarshingare16 <sagargshingare16@gmail.com>
2024-08-11 00:44:58 +0530

    config sever implementation

---
+++ Microservice-03-config-client/src/main/java/com/example/microservice/Microservice_03_config_client/Microservice03ConfigClientApplication.java
@@ -0,0 +1,11 @@
+package com.example.microservice.Microservice_03_config_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice03ConfigClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice03ConfigClientApplication.class, args);
+	}
+}
---
+++ Microservice-03-config-client/src/test/java/com/example/microservice/Microservice_03_config_client/Microservice03ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.Microservice_03_config_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice03ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
